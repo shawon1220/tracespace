68503c2d3c34a22908b4eef1263a090ddab3f442
shawon.xiao <shawon.xiao@allchips.com>
2023-02-08 10:28:48 +0800

    export { stringifySvg } from './stringify-svg' fixed

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,26 +1,27 @@
-import {s} from 'hastscript'
+import { s } from 'hastscript'
 
 import * as parser from '@tracespace/parser'
 import * as plotter from '@tracespace/plotter'
 import * as renderer from '@tracespace/renderer'
-import {random as randomId} from '@tracespace/xml-id'
-import {SIDE_TOP, SIDE_BOTTOM} from '@tracespace/identify-layers'
+import { random as randomId } from '@tracespace/xml-id'
+import { SIDE_TOP, SIDE_BOTTOM } from '@tracespace/identify-layers'
 
-import type {GerberTree} from '@tracespace/parser'
-import type {ImageTree} from '@tracespace/plotter'
-import type {GerberType, GerberSide} from '@tracespace/identify-layers'
-import type {SvgElement, ViewBox} from '@tracespace/renderer'
+import type { GerberTree } from '@tracespace/parser'
+import type { ImageTree } from '@tracespace/plotter'
+import type { GerberType, GerberSide } from '@tracespace/identify-layers'
+import type { SvgElement, ViewBox } from '@tracespace/renderer'
 
-import {readFile} from './read-file'
-import {determineLayerTypes} from './determine-layer-types'
-import {plotBoardShape, renderBoardShape} from './board-shape'
-import {getDrillLayers, getSideLayers} from './sort-layers'
-import {stringifySvg} from './stringify-svg'
+import { readFile } from './read-file'
+import { determineLayerTypes } from './determine-layer-types'
+import { plotBoardShape, renderBoardShape } from './board-shape'
+import { getDrillLayers, getSideLayers } from './sort-layers'
+import { stringifySvg } from './stringify-svg'
 
-import type {ParsedLayer} from './determine-layer-types'
-import type {BoardShape, BoardShapeRender} from './board-shape'
-import type {Side, SideLayers} from './sort-layers'
+import type { ParsedLayer } from './determine-layer-types'
+import type { BoardShape, BoardShapeRender } from './board-shape'
+import type { Side, SideLayers } from './sort-layers'
 
+export { stringifySvg } from './stringify-svg'
 
 export interface Layer {
   id: string
@@ -46,7 +47,7 @@ export interface RenderLayersResult {
   boardShapeRender: BoardShapeRender
 }
 
-export interface RenderBoardResult extends Record<Side, SvgElement> {}
+export interface RenderBoardResult extends Record<Side, SvgElement> { }
 
 export interface BoardShapeRenderFragment {
   viewBox: ViewBox
@@ -69,57 +70,57 @@ export async function read(files: File[] | string[]): Promise<ReadResult> {
   const layers: Layer[] = []
   const parseTreesById: Record<string, GerberTree> = {}
 
-  for (const {id, filename, parseTree} of parsedLayers) {
-    const {type, side} = layerTypesById[id]
+  for (const { id, filename, parseTree } of parsedLayers) {
+    const { type, side } = layerTypesById[id]
 
-    layers.push({id, filename, type, side})
+    layers.push({ id, filename, type, side })
     parseTreesById[id] = parseTree
   }
 
-  return {layers, parseTreesById}
+  return { layers, parseTreesById }
 }
 
 async function readAndParseFile(file: File | string): Promise<ParsedLayer> {
   const id = randomId()
-  const {filename, contents} = await readFile(file)
+  const { filename, contents } = await readFile(file)
   const parseTree = parser.parse(contents)
 
-  return {id, filename, parseTree}
+  return { id, filename, parseTree }
 }
 
 export function plot(readResult: ReadResult): PlotResult {
-  const {layers, parseTreesById} = readResult
+  const { layers, parseTreesById } = readResult
   const plotTreesById: Record<string, ImageTree> = {}
 
-  for (const {id} of layers) {
+  for (const { id } of layers) {
     plotTreesById[id] = plotter.plot(parseTreesById[id])
   }
 
   const boardShape = plotBoardShape(layers, plotTreesById, 0.02)
 
-  return {layers, plotTreesById, boardShape}
+  return { layers, plotTreesById, boardShape }
 }
 
 export function renderLayers(plotResult: PlotResult): RenderLayersResult {
-  const {layers, boardShape, plotTreesById} = plotResult
+  const { layers, boardShape, plotTreesById } = plotResult
   const boardShapeRender = renderBoardShape(boardShape)
   const rendersById: Record<string, SvgElement> = {}
 
-  for (const {id} of layers) {
+  for (const { id } of layers) {
     rendersById[id] = renderer.render(
       plotTreesById[id],
       boardShapeRender.viewBox
     )
   }
 
-  return {layers, rendersById, boardShapeRender}
+  return { layers, rendersById, boardShapeRender }
 }
 
 export function renderBoard(
   renderLayersResult: RenderLayersResult
 ): RenderBoardResult {
-  const {layers, rendersById, boardShapeRender} = renderLayersResult
-  const {viewBox, path: shapeRender} = boardShapeRender
+  const { layers, rendersById, boardShapeRender } = renderLayersResult
+  const { viewBox, path: shapeRender } = boardShapeRender
   const drillLayers = getDrillLayers(layers)
 
   const [x, y, width, height] = viewBox
@@ -157,28 +158,28 @@ export function renderBoard(
       },
       [
         s('defs', [
-          s('mask', {id: drillMaskId}, [
-            s('rect', {x, y, width, height, fill: '#fff'}),
-            s('g', {color: '#000'}, drillLayers.flatMap(getRenderChildren)),
+          s('mask', { id: drillMaskId }, [
+            s('rect', { x, y, width, height, fill: '#fff' }),
+            s('g', { color: '#000' }, drillLayers.flatMap(getRenderChildren)),
           ]),
-          s('mask', {id: resistMaskId}, [
-            s('rect', {x, y, width, height, fill: '#fff'}),
-            s('g', {color: '#000'}, resistLayers.flatMap(getRenderChildren)),
+          s('mask', { id: resistMaskId }, [
+            s('rect', { x, y, width, height, fill: '#fff' }),
+            s('g', { color: '#000' }, resistLayers.flatMap(getRenderChildren)),
           ]),
           shapeRender === undefined
             ? undefined
-            : s('clipPath', {id: shapeClipId}, shapeRender),
+            : s('clipPath', { id: shapeClipId }, shapeRender),
         ]),
-        s('g', {transform, 'clip-path': clipPath}, [
-          s('g', {mask: `url(#${drillMaskId})`}, [
-            s('rect', {fill: '#666', x, y, width, height}),
-            s('g', {color: '#c93'}, copperLayers.flatMap(getRenderChildren)),
+        s('g', { transform, 'clip-path': clipPath }, [
+          s('g', { mask: `url(#${drillMaskId})` }, [
+            s('rect', { fill: '#666', x, y, width, height }),
+            s('g', { color: '#c93' }, copperLayers.flatMap(getRenderChildren)),
           ]),
-          s('g', {mask: `url(#${resistMaskId})`}, [
-            s('rect', {fill: '#004200', opacity: '0.8', x, y, width, height}),
-            s('g', {color: '#fff'}, silkLayers.flatMap(getRenderChildren)),
+          s('g', { mask: `url(#${resistMaskId})` }, [
+            s('rect', { fill: '#004200', opacity: '0.8', x, y, width, height }),
+            s('g', { color: '#fff' }, silkLayers.flatMap(getRenderChildren)),
           ]),
-          s('g', {color: '#999'}, pasteLayers.flatMap(getRenderChildren)),
+          s('g', { color: '#999' }, pasteLayers.flatMap(getRenderChildren)),
         ]),
       ]
     )
@@ -188,8 +189,8 @@ export function renderBoard(
 }
 
 export function renderFragments(plotResult: PlotResult): RenderFragmentsResult {
-  const {layers, plotTreesById, boardShape} = plotResult
-  const {viewBox, path: boardShapePath} = renderBoardShape(boardShape)
+  const { layers, plotTreesById, boardShape } = plotResult
+  const { viewBox, path: boardShapePath } = renderBoardShape(boardShape)
   const topLayers = getSideLayers(SIDE_TOP, layers)
   const bottomLayers = getSideLayers(SIDE_BOTTOM, layers)
   const drillLayers = getDrillLayers(layers)
@@ -201,7 +202,7 @@ export function renderFragments(plotResult: PlotResult): RenderFragmentsResult {
 
   const svgFragmentsById: Record<string, string> = {}
 
-  for (const {id} of layers) {
+  for (const { id } of layers) {
     svgFragmentsById[id] = stringifySvg(
       renderer.renderFragment(plotTreesById[id])
     )
